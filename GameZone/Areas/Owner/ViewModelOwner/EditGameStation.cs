982befba15f8ed88f88de4cdcf7b7396bf856f83
AmmarBawazirTech <AmmarBawazirTech@gmail.com>
2025-06-07 16:36:56 +0300

    Create The Owner Station Edit Station Page With its logic And Create the App DB Context With The Models and

---
+++ GameZone/Areas/Owner/ViewModelOwner/EditGameStation.cs
@@ -0,0 +1,20 @@
+﻿using GameZone.Helpers.Attributes;
+using GameZone.Helpers;
+using GameZone.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace GameZone.Areas.Owner.ViewModelOwner
+{
+    public class EditGameStation : GameStationVM
+    {
+        public int Id { get; set; }
+        public string? CurrentCover { get; set; }
+        [Display(Name = "Image")]
+        [Required(ErrorMessage = "Please, Insert an Image!.")]
+
+        [AllowedExtensions(FileSettings.AllowedExtensions)]
+        [MaxFileSize(FileSettings.MaxFileSizeInBytes)]
+        public IFormFile Cover { get; set; } = default!;
+        public string? Image { get; set; }
+    }
+}
---
+++ GameZone/Data/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using GameZone.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameZone.Data;
+
+public class AppDBContext : IdentityDbContext<AppUser>
+{
+    public AppDBContext(DbContextOptions<AppDBContext> options)
+    : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+    }
+
+    public DbSet<Game> Games { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<GameStation> GameStations { get; set; }
+    public DbSet<Rating> Ratings { get; set; }
+    public DbSet<Room> Rooms { get; set; }
+    public DbSet<Device> Devices { get; set; }
+    public DbSet<GameStationGame> GameStationGames { get; set; }
+    public DbSet<PaymentMethod> PaymentMethods { get; set; }
+    public DbSet<Reservation> Reservations { get; set; }
+    public DbSet<RoomsPicture> RoomsPictures { get; set; }
+}
